9c9b4a765ac5473a050755c28d645100ba2e0d8b
Zaiko Euheni <rahunak@yandex.by>
2022-05-23 14:17:16 +0300

    fix: rewrite 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,28 +4,40 @@ const path = require('path');
 const origin = path.join(__dirname,'files');
 const copy =path.join(__dirname,'files-copy');
 
-async function removeFolder(){
+async function removeFolder(where){
   return fs.promises
-    .rm(copy,{recursive:true,force:true})
+    .rm(where,{recursive:true,force:true})
     .catch(err=>console.log('Err removeFolder',err.message));
 }
-async function createCopyFolder(){
-  fs.promises.mkdir(copy, { recursive: true })
-    .catch(err=>console.log('Err createCopyFolder',err.message));
+
+async function createFolder(where){
+  return  fs.promises.mkdir(where, { recursive: true })
+    .catch(err=>console.log('Err createFolder ',err.message));
 }
-async function copyFiles(){
-  fs.promises.readdir(origin,{ withFileTypes: true })
+
+async function copyFiles(from,to){
+  return fs.promises.readdir(from,{ withFileTypes: true })
     .then(files=>{
       for(let file of files){
-        fs.promises.copyFile(path.join(origin,file.name),
-          path.join(copy,file.name))
-          .catch(err=>console.log('Err copyFile',err.message));
+
+        if(file.isFile()){
+          //Аналог из задачи 4
+          fs.promises.copyFile(path.join(from,file.name),
+            path.join(to,file.name))
+            .catch(err=>console.log('Err copyFile',err.message));
+        }else if(file.isDirectory()){
+          //Если это папка то создаем новую папку и не забываем что в ней могут быть еще файлы => рекурсивно запускаем нашу функцию
+          createFolder(path.join(__dirname,file.name))
+            .then(()=>copyFiles(
+              path.join(__dirname,file.name),
+              path.join(__dirname,'files-copy',file.name)
+            ));
+        }
       }
     })
     .catch(err=>console.log('Err readdir',err.message));
 }
 
-removeFolder().then(()=>{
-  createCopyFolder();
-  copyFiles();
-});
+removeFolder(copy)
+  .then(()=>createFolder(copy))
+  .then(()=>copyFiles(origin,copy));
